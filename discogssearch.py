ef83fce67998b3027e715230e7d3884e548e4aec
keigo <keigox68000@gmail.com>
2024-03-27 14:22:48 +0900

    output folder added

--- discogssearch.py
+++ discogssearch.py
@@ -12,6 +12,11 @@ ACCESS_TOKEN = config['discogs']['access_token']
 USER_AGENT = 'ExampleApplication/0.1'
 d = discogs_client.Client(USER_AGENT, user_token=ACCESS_TOKEN)
 
+# 画像を保存するフォルダの設定
+output_folder = 'output'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 def search_album_and_save_art(keyword):
     """キーワードで検索し、最初のリリースのアルバムアートを保存します。"""
     try:
@@ -22,7 +27,7 @@ def search_album_and_save_art(keyword):
             album_title = result.title
             primary_image = next((image for image in result.images if image['type'] == 'primary'), None)
             if primary_image:
-                filename = f"{album_title}-{artist_name}".replace("/", "-") # スラッシュを避ける
+                filename = f"{album_title}-{artist_name}".replace("/", "-")  # スラッシュを避ける
                 save_album_art(primary_image['uri'], filename)
             else:
                 print(f"No primary image available for {keyword}")
@@ -32,7 +37,7 @@ def search_album_and_save_art(keyword):
         print(f"Error processing {keyword}: {e}")
 
 def save_album_art(image_url, filename):
-    """アルバムアートをダウンロードして保存します。"""
+    """アルバムアートをダウンロードしてoutputフォルダに保存します。"""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Referer': 'https://www.discogs.com/'
@@ -40,8 +45,8 @@ def save_album_art(image_url, filename):
     try:
         response = requests.get(image_url, headers=headers)
         response.raise_for_status()
-        valid_filename = "".join(char for char in filename if char.isalnum() or char in " _-").replace("/", "-") # スラッシュを避ける
-        file_path = os.path.join('.', f'{valid_filename}.jpg')
+        valid_filename = "".join(char for char in filename if char.isalnum() or char in " _-").replace("/", "-")  # スラッシュを避ける
+        file_path = os.path.join(output_folder, f'{valid_filename}.jpg')
         with open(file_path, 'wb') as file:
             file.write(response.content)
         print(f'Album art saved to {file_path}')
@@ -57,4 +62,3 @@ with open('result.txt', 'r') as file:
             search_album_and_save_art(keyword)
         else:
             print(f"Invalid line format: {line}")
-            
